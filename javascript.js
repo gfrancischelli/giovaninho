0ff02d87042a4dabfa84fefd8a9dc4ff5fd10ae5
Giovanni Francischelli <gifrancischelli@gmail.com>
2017-06-25 20:46:40 -0300

    :sparkles: XML funcionando

--- javascript.js
+++ javascript.js
@@ -1,29 +1,30 @@
+// TA VENO O NOME DA FORM AQUI
+document.forms.nomeDaForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  console.log(e.target);
 
+  // "this" no caso é o formulário onde colocamos o event listener
+  // nomeDoInput é a propriedade "name" do input lá
+  var input = this.nomeDoInput;
 
-function Macaco(){
-    
-    document.getElementById("formID").addEventListener("submit", function(e) {
-    e.preventDefault();
-    console.log(e.target);
-        
-    var Birl = document.getElementById("TesteID") ;
-    
-    var xhr = new XMLHttpRequest();
-    
-    var http = "https://images-api.nasa.gov/search?q=" ;
-    
-    xhr.open("GET", http + Birl.value.toLowerCase() , true );
-    
-    xhr.send(Birl.value);   
-               
-});    
-               
-   
-       
-    
-    
-}
+  var xhr = new XMLHttpRequest();
+  var http = "https://images-api.nasa.gov/search?q=";
 
+  xhr.open("GET", http + input.value.toLowerCase(), true);
 
+  // Executa a chamada. Usa os event listener pra acompanhar o status
+  // "load" quando a chamada for completa
+  // também tem "progress", "error", "abort"
+  xhr.send();
 
-     
+  // Recebe a mensagem do XHR e lida com ela
+  function handleResponse(response) {
+    console.log(response.target.response);
+  }
+
+  // Registra esse callback pra quando o xhr responder
+  xhr.addEventListener("load", handleResponse, false);
+
+  // Um link bom pra ler
+  // https://davidwalsh.name/xmlhttprequest
+});
